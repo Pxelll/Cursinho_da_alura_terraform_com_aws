478c6b782cb311390caeec25b9d27d7513bbba45
Thiago Adam <thiagohsm2009@hotmail.com>
2023-06-07 18:34:24 -0300

    added user_data to main.tf

--- main.tf
+++ main.tf
@@ -13,10 +13,17 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "name" {
+resource "aws_instance" "maquina_virtual" {
   ami = "ami-0261755bbcb8c4a84"
   instance_type = "t2.micro"
   key_name = "terraform_alura"
+  #Esses comandos vão ser executados da mesma forma que um script seria executado, então é interessante começarmos com #!/bin/bash para identificar com qual tipo de console queremos executá-lo.
+  user_data = <<-EOF
+                 #!/bin/bash
+                 cd /home/ubuntu
+                 echo "<h1>Feito com terraform</h1>" > index.html
+                 nohup busybox httpd -f -p 8080 &
+                 EOF
   tags = {
     Name = "Alura_Terraform"
   }

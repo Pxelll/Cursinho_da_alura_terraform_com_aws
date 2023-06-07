6eb681a47ba000b571a5efc715ac43070cfa2c30
Thiago Adam <thiagohsm2009@hotmail.com>
2023-06-06 23:56:46 -0300

    added aws_key to instance

--- main.tf
+++ main.tf
@@ -16,6 +16,7 @@ provider "aws" {
 resource "aws_instance" "name" {
   ami = "ami-0261755bbcb8c4a84"
   instance_type = "t2.micro"
+  key_name = "terraform_alura"
   tags = {
     Name = "Alura_Terraform"
   }
